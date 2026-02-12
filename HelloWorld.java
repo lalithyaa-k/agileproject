d51a688237e455acb997abc2060a952296fb7bfb
vaishnavig2023a <vaishnavi.g2023a@vitstudent.ac.in>
2026-02-12 22:54:55 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Mini CI Project Working!");
+        System.out.println("To test flow of pushing in github");
     }
 }
